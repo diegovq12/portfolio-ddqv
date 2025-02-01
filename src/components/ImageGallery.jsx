e221fde48d072889b74699b3fcb05c5820339422
diegod <diegovq12@gmail.com>
2025-01-31 23:13:19 -0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/ImageGallery.jsx 	modified:   src/components/header.astro

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IoIosArrowBack , IoIosArrowForward  } from 'react-icons/io';
 
 const ImageGallery = ({ images }) => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -13,14 +14,14 @@ const ImageGallery = ({ images }) => {
 
     return (
         <div className="image-gallery" style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "1rem" }}>
-            <button className="arrow left" onClick={previousImage}> &lt; </button>
+            <button className="arrow left" onClick={previousImage}><IoIosArrowBack /></button>
             <img
                 src={images[currentImageIndex]}
                 alt={`Imagen ${currentImageIndex + 1} del proyecto`}
                 aria-label={`Imagen ${currentImageIndex + 1} de ${images.length}`}
                 style={{
-                    maxWidth: "30rem", 
-                    height: "20rem",   
+                    maxWidth: "28rem", 
+                    height:"20rem",
                     borderRadius: "8px",
                     objectFit: "fill",
                     padding:"1rem",
@@ -28,7 +29,30 @@ const ImageGallery = ({ images }) => {
                     marginRight: "auto"
                 }}
             />
-            <button className="arrow right" onClick={nextImage}> &gt; </button>
+            <button className="arrow right" onClick={nextImage}><IoIosArrowForward /></button>
+            <style jsx>{`
+                .arrow {
+                    background-color: transparent;
+                    border: none;
+                    font-size: 2rem;
+                    cursor: pointer;
+                    color: #fff;
+                    transition: color 0.3s ease;
+                }
+
+                .arrow:hover {
+                    color: #456a8d;
+                }
+
+                .left {
+                    padding:1rem;
+                    margin-right:-10px;
+                }
+
+                .right {
+                    margin-left: -20px;
+                }
+            `}</style>
         </div>
     );
 };
